6c225c2394eae72b06620acbccafdec0c743a281
kozinov.av <kozinov.av@phystech.edu>
2019-01-12 17:30:51 +0300

    Add some changes

--- main.cpp
+++ main.cpp
@@ -6,29 +6,23 @@
 #include <alcommon/albrokermanager.h>
 #include <alcommon/altoolsmain.h>
 
-#if MOVEMENTGRAPH_IS_REMOTE
+#ifdef MOVEMENTGRAPH_IS_REMOTE
 # include "graphcreator.h"
-# define ALCALL
 #else
 # include "movementgraph.h"
-# ifdef _WIN32
-#  define ALCALL __declspec(dllexport)
-# else
-#  define ALCALL
-# endif
 #endif
 
 
 extern "C"
 {
-  ALCALL int _createModule(boost::shared_ptr<AL::ALBroker> pBroker)
+  int _createModule(boost::shared_ptr<AL::ALBroker> pBroker)
   {
     // init broker with the main broker instance
     // from the parent executable
     AL::ALBrokerManager::setInstance(pBroker->fBrokerManager.lock());
     AL::ALBrokerManager::getInstance()->addBroker(pBroker);
     // create module instances
-#if MOVEMENTGRAPH_IS_REMOTE
+#ifdef MOVEMENTGRAPH_IS_REMOTE
     AL::ALModule::createModule<GraphCreator>(pBroker, "GraphCreator");
 #else
     AL::ALModule::createModule<MovementGraph>(pBroker, "MovementGraph");
@@ -36,7 +30,7 @@ extern "C"
     return 0;
   }
 
-  ALCALL int _closeModule(  )
+  int _closeModule(  )
   {
     return 0;
   }
